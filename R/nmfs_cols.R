02f96ba419fff26e63b1770b59e50f8e9b45f954
ChristineStawitz-NOAA <christine.stawitz@noaa.gov>
2020-11-23 16:29:25 -0800

    add ggplot2 functions to namespace

--- R/nmfs_cols.R
+++ R/nmfs_cols.R
@@ -102,7 +102,7 @@ display_nmfs_palette <- function(name, n, ...) {
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments passed to discrete_scale() or
 #'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
-#' @import ggplot2 
+#' @importFrom ggplot2 ggplot geom_point discrete_scale scale_color_gradientn
 #'@examples
 #'  p <- ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +
 #'  geom_point(size = 4) +
@@ -125,7 +125,7 @@ scale_color_nmfs <- function(palette = "oceans", discrete = TRUE, reverse = FALS
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments passed to discrete_scale() or
 #'            scale_fill_gradientn(), used respectively when discrete is TRUE or FALSE
-#' @import ggplot2
+#' @importFrom ggplot2 ggplot geom_bar theme discrete_scale scale_fill_gradientn
 #'@examples
 #'  ggplot(mpg, aes(manufacturer, fill = manufacturer)) +
 #'  geom_bar() +
