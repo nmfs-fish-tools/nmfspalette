cf78f7032fc8df2c041bbf2c37ebfbdd364f7b5b
Bai-Li-NOAA <Bai-Li-NOAA@users.noreply.github.com>
2022-01-19 22:07:55 +0000

    style and docs: run devtools::document() and styler::style_pkg()

--- R/nmfspalette.R
+++ R/nmfspalette.R
@@ -1,8 +1,8 @@
 #' nmfspalette: A package for NOAA Fisheries color schemes
 #'
-#' The nmfspalette package provides functions to extract colors, color palettes, and use ggplot2 
+#' The nmfspalette package provides functions to extract colors, color palettes, and use ggplot2
 #' palettes in R.
-#'  
+#'
 #' @section Details:
 #' This package contains functions to support graphing in base R and ggplot2 using NOAA Fisheries
 #' colors.
