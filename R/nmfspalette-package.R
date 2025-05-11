51ad1894689bd625632fe71f14f7d9ea2da9d8ea
njtierney <nicholas.tierney@gmail.com>
2025-05-11 11:38:52 +1000

    use_package_doc()

---
+++ R/nmfspalette-package.R
@@ -0,0 +1,14 @@
+#' nmfspalette: A package for NOAA Fisheries color schemes
+#'
+#' The nmfspalette package provides functions to extract colors, color
+#' palettes, and use ggplot2 palettes in R.
+#'
+#' @section Details:
+#' This package contains functions to support graphing in base R and ggplot2
+#' using NOAA Fisheries colors.
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
